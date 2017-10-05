cb3a0f4ef40b115af81b410b35c37cae0c0c5007
Raul6469 <24607388+Raul6469@users.noreply.github.com>
2017-10-05 15:48:32 +0200

    Operations class

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,13 @@
+import tools.Operations;
 
 public class HelloWorld {
 
 	public static void main (String [] args)
 	{
 		System.out.println("Hello World !");
+		int val = Operations.somme(2, 3);
+		
+		System.out.println("Somme = " + val);
 	}
 	
 }
---
+++ src/tools/Operations.java
@@ -0,0 +1,13 @@
+package tools;
+
+public class Operations {
+	public static int somme(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int soustraction(int a, int b)
+	{
+		return a-b;
+	}
+}
